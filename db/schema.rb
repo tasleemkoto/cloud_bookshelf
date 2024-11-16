d083f9c17c9731f71f8850bcd3f50e7d145e96d1
Astride Sardouce <astridesardouce@gmail.com>
2024-11-16 10:46:31 +0400

    edit

--- db/schema.rb
+++ db/schema.rb
@@ -14,25 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_14_171659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.text "summary"
-    t.string "author"
-    t.string "genre"
-    t.integer "year"
-    t.string "format"
-    t.boolean "availability", default: true
-    t.string "location"
-    t.string "qr_code"
-    t.string "photo"
-    t.integer "quantity"
-    t.string "status"
-    t.integer "view_count", default: 0
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_books_on_user_id"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,6 +42,26 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_14_171659) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "summary"
+    t.string "author"
+    t.string "genre"
+    t.integer "year"
+    t.string "format"
+    t.boolean "availability", default: true
+    t.string "location"
+    t.string "qr_code"
+    t.string "photo"
+    t.integer "quantity"
+    t.string "status"
+    t.integer "view_count", default: 0
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -73,7 +74,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_14_171659) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "books", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "books", "users"
 end
