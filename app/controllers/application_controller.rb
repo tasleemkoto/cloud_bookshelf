cfe58a6539d2610f0f497fcd0c84561880e14834
vidoor mathoora <shyamalmathoora@gmail.com>
2024-11-30 12:18:34 +0400

    Adding_dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,14 @@ class ApplicationController < ActionController::Base
     redirect_to(root_path)
   end
 
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admin_dashboard_path
+    # else
+    #   user_dashboard
+    end
+  end
+
   private
 
   def skip_pundit?
