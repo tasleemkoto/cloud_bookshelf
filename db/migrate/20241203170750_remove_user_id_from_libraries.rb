2fc8590f97203035bab720d5f4e4d5ba4a4b9621
vidoor mathoora <shyamalmathoora@gmail.com>
2024-12-05 19:16:42 +0400

    CorrectedSchema

--- db/migrate/20241203170750_remove_user_id_from_libraries.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromLibraries < ActiveRecord::Migration[7.1]
-  def change
-    change_column_default :libraries, :user_id, nil
-  end
-end
--- db/migrate/20241204105744_remove_default_from_user_in_libraries.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDefaultFromUserInLibraries < ActiveRecord::Migration[7.1]
-  def change
-    change_column_default :libraries, :user_id, from: User.first.id, to: nil
-  end
-end
