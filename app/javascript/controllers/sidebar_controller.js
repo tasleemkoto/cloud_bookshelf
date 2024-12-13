d0c86e97dd163031e1401bdc82c6aa4b7a4f6aec
vidoor mathoora <shyamalmathoora@gmail.com>
2024-12-13 17:22:11 +0400

    linkToBeAdded

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -5,18 +5,19 @@ export default class extends Controller {
     console.log("Hello World!")
   }
   showSidebar() {
-      console.log("sidebar has been clicked")
       const toggle = document.getElementById("header-toggle")
       const sidebar = document.getElementById("sidebar")
-      console.log(toggle)
-      console.log(sidebar)
-      if(toggle && sidebar){
-        sidebar.classList.toggle('show-sidebar')
+      const header = document.getElementById("header")
+      const main = document.getElementById("main")
+     
           toggle.addEventListener('click', ()=>{
               /* Show */
               console.log("sidebar has been clicked")
               sidebar.classList.toggle('show-sidebar')
+              
+              header.classList.toggle('left-pd')
+
+              main.classList.toggle('left-pd')
           })
-      }
-  }
+      } 
 }
