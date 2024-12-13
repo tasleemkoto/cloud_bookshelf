c4f44fdf5d0172f9fd9ea6894bfcaaf98340a060
vidoor mathoora <shyamalmathoora@gmail.com>
2024-12-13 21:46:05 +0400

    finalT

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,23 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
-  connect() {
-    console.log("Hello World!")
-  }
-  showSidebar() {
-      const toggle = document.getElementById("header-toggle")
-      const sidebar = document.getElementById("sidebar")
-      const header = document.getElementById("header")
-      const main = document.getElementById("main")
+// export default class extends Controller {
+//   connect() {
+//     console.log("Hello World!")
+//   }
+//   showSidebar() {
+//       const toggle = document.getElementById("header-toggle")
+//       const sidebar = document.getElementById("sidebar")
+//       const header = document.getElementById("header")
+//       const main = document.getElementById("main")
      
-          toggle.addEventListener('click', ()=>{
-              /* Show */
-              console.log("sidebar has been clicked")
-              sidebar.classList.toggle('show-sidebar')
+//           toggle.addEventListener('click', ()=>{
+//               /* Show */
+//               console.log("sidebar has been clicked")
+//               sidebar.classList.toggle('show-sidebar')
               
-              header.classList.toggle('left-pd')
+//               header.classList.toggle('left-pd')
 
-              main.classList.toggle('left-pd')
-          })
-      } 
-}
+//               main.classList.toggle('left-pd')
+//           })
+//       } 
+// }
